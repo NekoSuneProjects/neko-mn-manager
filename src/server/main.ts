7ce6c0e5030c0702df0000cb9c75f8ff03daed8d
NekoSuneVR <chisdeal2014@hotmail.com>
2025-12-24 18:47:12 +0000

    auto porting Ptderactyl

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,12 @@
 import { NodeManager } from "../manager.ts";
 import { startServer } from "./server.ts";
 
-const port = Number(process.env.PORT ?? 8080);
+const port = Number(
+  process.env.PORT ??
+    process.env.SERVER_PORT ??
+    process.env.PTERODACTYL_SERVER_PORT ??
+    8080
+);
 
 const manager = new NodeManager();
 await manager.init();
